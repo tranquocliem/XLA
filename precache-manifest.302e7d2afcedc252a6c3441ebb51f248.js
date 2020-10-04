9fd59cd84d78175188253d045cf61e8d300eeca3
lnnd <lnnduy68@gmail.com>
2020-10-04 12:57:54 +0700

    Updates

---
+++ precache-manifest.302e7d2afcedc252a6c3441ebb51f248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6772b2d87478bcea39ee6ebc11d01d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a9e7e1760f41fc6a94d",
+    "url": "/static/css/main.8f875ee8.chunk.css"
+  },
+  {
+    "revision": "ff4ca9b1394f747198f7",
+    "url": "/static/js/2.a214dd91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a214dd91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9e7e1760f41fc6a94d",
+    "url": "/static/js/main.27ee477b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
